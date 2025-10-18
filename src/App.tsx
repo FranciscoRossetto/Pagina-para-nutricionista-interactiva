5114aae00afbaa94e0d3bb8f042781f0025ac663
FranciscoRossetto <fross2006@example.com>
2025-10-18 16:36:34 -0300

    Implementacion de react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import { useState } from "react";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
+import { Home } from "./pages/Home";
+import { Recetas } from "./pages/Recetas";
+import { Agenda } from "./pages/Agenda";
 import { MoreLessGame } from "./pages/MoreLessGame";
 
-export default function App() {
+function App() {
   return (
-
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/recetas" element={<Recetas />} />
+        <Route path="/agenda" element={<Agenda />} />
+        <Route path="/juego" element={<MoreLessGame />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
+
+export default App;
