9a3ebf2b6d39cf2571c23bd4f14f5e7bf9a85425
FranciscoRossetto <fross2006@example.com>
2025-10-30 18:50:02 -0300

    AHORA?? NETLIFY

--- Frontend_React/src/config/api.ts
+++ Frontend_React/src/config/api.ts
@@ -1,2 +1,7 @@
+// Tomamos la URL desde VITE_API_URL o usamos localhost
 export const API = import.meta.env.VITE_API_URL || "http://localhost:4000";
-console.log("API base URL:", API);
+
+// Quitamos slash final si lo tuviera
+export const BASE_URL = API.replace(/\/$/, "");
+
+console.log("API base URL:", BASE_URL);
